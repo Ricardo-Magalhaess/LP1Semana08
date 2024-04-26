2dcf0bc57d15681a13ec06ccdd1e305297f6c548
Ricardo Magalhães <ricardomagalhaes.dev@gmail.com>
2024-04-26 10:34:48 +0100

    Foi criada a interface ICanFly e foi adicionada a classe Bat e Bee

--- AnimalKingdom/Bat.cs
+++ AnimalKingdom/Bat.cs
@@ -5,9 +5,10 @@ using System.Threading.Tasks;
 
 namespace AnimalKingdom
 {
-    public class Bat : Animal, IMammal
+    public class Bat : Animal, IMammal, ICanFly
     {
         public int NumberOfNipples {get; } =2;
+        public int NumberOfWings {get; } = 2;
         public override string Sound()
         {
             return base.Sound() + "micmicmic";
--- AnimalKingdom/Bee.cs
+++ AnimalKingdom/Bee.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 
 namespace AnimalKingdom
 {
-    public class Bee : Animal
+    public class Bee : Animal, ICanFly
     {
+        public int NumberOfWings { get; } = 4;
+
         public override string Sound()
         {
             return base.Sound() + "ZZZZZ";
